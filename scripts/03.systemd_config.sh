6126daa54c438b9ec034dc106e2b72dfe53420ef
Paweł Wanat <wanatpj@gmail.com>
2022-01-03 01:12:46 +0100

    restart daemon

--- scripts/03.systemd_config.sh
+++ scripts/03.systemd_config.sh
@@ -1,6 +1,6 @@
 set -x
 
-read -r -d '' SYSTEMD_CONFIG <<- EOM
+SYSTEMD_CONFIG=$(cat << EOM
 [Unit]
 Description=Reverse SSH connection
 After=network.target
@@ -8,9 +8,14 @@ After=network.target
 [Service]
 ExecStart=/usr/bin/ssh -R 12345:localhost:22 -p $REMOTE_PORT -nfN $REMOTE_ADDR
 Restart=always
+User=$(whoami)
 
 [Install]
 WantedBy=default.target
 EOM
+)
 
-echo $SYSTEMD_CONFIG | sudo tee /etc/systemd/system/remote_node_connect.service
+echo "$SYSTEMD_CONFIG" | sudo tee /etc/systemd/system/remote_node_connect.service
+
+sudo systemctl daemon-reload
+sudo systemctl start remote_node_connect.service
